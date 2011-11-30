1eda6dd818df95ef9814bdcd725f2262bb1ee8cb
dennyabraham <email@dennyabraham.com>
2011-11-30 15:49:16 -0500

    add positions to home controller and add past positions to api controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+  def index
+    @dog = Dog.first
+    @positions = @dog.past_positions.limit(5).collect do |position|
+      {
+        :x => position.x,
+        :y => position.y
+      }
+    end
+  end
 end
--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,9 +1,15 @@
 class PositionsController < InheritedResources::Base
   belongs_to :dog
-  custom_actions :resource => :current
+  custom_actions :resource => :current, :collection => :past
   def current
     @dog = Dog.find(params[:dog_id])
     @position = @dog.current_position
     show!
+  end
+
+  def past
+    @dog = Dog.find(params[:dog_id])
+    @positions = @dog.past_positions
+    index!
   end
 end
