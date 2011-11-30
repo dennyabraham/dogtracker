f3bd39691b398f41049790c09ae4bf9d14f9e317
dennyabraham <email@dennyabraham.com>
2011-11-30 15:48:41 -0500

    add past positions to dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,10 @@ class Dog < ActiveRecord::Base
   has_many :positions
 
   def current_position
-    self.positions.where("time <= :now", :now => DateTime.now).order('date DESC').last
+    self.positions.where("time <= :now", :now => DateTime.now).order('time DESC').last
+  end
+
+  def past_positions
+    self.positions.where("time <= :now", :now => DateTime.now).order('time DESC')
   end
 end
