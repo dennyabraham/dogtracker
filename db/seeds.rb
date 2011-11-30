df285c99b06c2f02b7a680d295dd26186c55b982
dennyabraham <email@dennyabraham.com>
2011-11-30 15:27:04 -0500

    increase position granularity

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 dog = Dog.create!(:name => 'Ezio')
-for i in 1..500 do
+for i in 1..10000 do
   x = Kernel.rand(400) + 50
   y = Kernel.rand(300) + 100
-  dog.positions.create!(:x => x, :y => y, :time => 18.hours.ago + i * 5.minutes)
+  dog.positions.create!(:x => x, :y => y, :time => 18.hours.ago + i * 15.seconds)
 end
