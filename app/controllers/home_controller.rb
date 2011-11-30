a420d96baca73a791ee8fe2db7ab8c3e9d9da92f
dennyabraham <email@dennyabraham.com>
2011-11-30 16:17:35 -0500

    pessimize rendering for dog tracker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 class HomeController < ApplicationController
   def index
     @dog = Dog.first
-    @positions = @dog.past_positions.limit(5).collect do |position|
-      {
-        :x => position.x,
-        :y => position.y
-      }
-    end
+    @positions = @dog.past_positions.limit(5)
   end
 end
