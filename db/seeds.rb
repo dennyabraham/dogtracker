90a14cae3a72d73abade78945c15fd162d351d66
dennyabraham <email@dennyabraham.com>
2011-11-30 15:56:22 -0500

    update seed granularity

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ dog = Dog.create!(:name => 'Ezio')
 for i in 1..10000 do
   x = Kernel.rand(400) + 50
   y = Kernel.rand(300) + 100
-  dog.positions.create!(:x => x, :y => y, :time => 18.hours.ago + i * 15.seconds)
+  dog.positions.create!(:x => x, :y => y, :time => 4.hours.ago + i * 5.seconds)
 end
